f8224ac64c84f17ef9a5514b1f05c22edcf3d041
Ryan Collins <ryan@ryanmcollins.com>
2017-06-28 15:35:54 -0400

    Updated Syntax to Match New Vapor

--- PerfectPress/Sources/JSONGenerator.swift
+++ PerfectPress/Sources/JSONGenerator.swift
@@ -1,19 +1,19 @@
 #if os(Linux)
-import Glibc
+    import Glibc
 #else
-import Darwin
+    import Darwin
 #endif
 
 struct JSONCreator {
-    func generateJSON() -> [String: Any] {
-
-        var finalJSON = [String: Any]()
-
+    func generateJSON() -> [String: Int] {
+        
+        var finalJSON = [String: Int]()
+        
         for i in 1...10 {
             let randomNumber = Int(arc4random_uniform(UInt32(1000)))
             finalJSON["Test Number \(i)"] = randomNumber
         }
-
+        
         return finalJSON
     }
 }
